6ac556756b5dc444925e7616193456e2fad6b029
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-13 14:26:14 +0000

    created model

--- todoapp/models.py
+++ todoapp/models.py
@@ -5,6 +5,15 @@ class Todo(models.Model):
     label = models.CharField(max_length=500)
     description = models.CharField(max_length=1000)
     category = models.ForeignKey(Users, on_delete=models.CASCADE)
+    LOW = 'LOW'
+    MEDIUM = 'MEDIUM'
+    URGENT = 'URGENT'
+    PRIORITY_CHOICES = [
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (URGENT, 'Urgent')
+    ]
+    priority = models.Charfield(max_length = 6, choices = PRIORITY_CHOICES, default = LOW)
 
 class Category(models.Model):
     label = models.CharField(max_length=250)
