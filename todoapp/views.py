7fe5101585cdc1a714658bf21953028ae3cd5b96
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-14 23:43:57 +0000

    updated views

--- todoapp/views.py
+++ todoapp/views.py
@@ -10,20 +10,20 @@ from .models import *
 
 class TodoViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Todos to be viewed or edited.
     """
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     # permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    # filterset_fields = ('user__first_name',)
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # /api/todos?user__first_name= 
+    filterset_fields = ('user__first_name',)
     # filterset_fields = ['user_id']
-    # search_fields = ['=user', 'User.first_name']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Categories to be viewed or edited.
     """
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -34,7 +34,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 class EventViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Events to be viewed or edited.
     """
     queryset = Event.objects.all()
     serializer_class = EventSerializer
@@ -44,11 +44,12 @@ class EventViewSet(viewsets.ModelViewSet):
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Users to be viewed or edited.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    # users/?search=
     search_fields = ['=first_name', 'first_name']
     
